af97a8a14f3507c554c295db8e2180f443c4762d
lynetteduran <lynetteduran@Lynettes-MacBook-Air.local>
2016-09-19 02:33:44 -0700

    Created README, html, js, and css files for 'Tile Slides' game under Sliding-Tiles-Game repository. This version of the game is MVPquality

---
+++ README.md
@@ -0,0 +1,4 @@
+Tile Slide Game
+
+On a 3x3 grid, there are 8 tiles numbered 1-8 and an empty tile. Upon page load, the tiles appear out of numerical order. You can only move a tile into the empty space if it borders the empty space either directly above, below, to the right or left. Click on the tile you wish to move into the empty space. Shift tiles one by one in efforts to position the tiles in ascending numerical order, starting left to right on row 1, then row 2, then row 3. You win when your tiles are in the abovementioned order with the last space on row 3 empty. 
+
---
+++ main.js
@@ -0,0 +1,107 @@
+var evaluateClick = true;
+var board;
+var emptyTile;
+
+function Tile(x,y,el) {
+  this.x = x;
+  this.y = y;
+  this.el = el;
+  this.applyPosition = function() {
+    this.el.style.left = this.x * 200 + "px";
+    this.el.style.top = this.y * 200 + "px";
+  }
+
+  var tile = this;
+  this.el.addEventListener('click', function(e) {
+    if (evaluateClick == false) {
+      return;
+    }
+    var xa = tile.x;
+    var xe = emptyTile.x;
+    var ya = tile.y;
+    var ye = emptyTile.y;
+    var dx = Math.abs(xa - xe);
+    var dy = Math.abs(ya - ye);
+    if ((dx == 0 && dy == 1) || (dx == 1 && dy == 0)) { 
+      emptyTile.x = xa;
+      emptyTile.y = ya;
+      tile.x = xe;
+      tile.y = ye;
+      updateBoard();
+    }
+  });
+}
+
+function updateBoard() {
+    board.forEach(function(tile) {
+      tile.applyPosition();
+    });
+    checkWin();
+}
+
+function checkWin() {
+   if  (tile0.x == 2 && tile0.y == 2
+    &&  tile1.x == 0 && tile1.y == 0
+    &&  tile2.x == 1 && tile2.y == 0
+    &&  tile3.x == 2 && tile3.y == 0
+    &&  tile4.x == 0 && tile4.y == 1
+    &&  tile5.x == 1 && tile5.y == 1
+    &&  tile6.x == 2 && tile6.y == 1
+    &&  tile7.x == 0 && tile7.y == 2
+    &&  tile8.x == 1 && tile8.y == 2) {
+    document.getElementById('headline').innerHTML = "YOU WON!";
+    document.getElementById('headline').style.visibility = "visible";
+    evaluateClick = false;
+  }
+}
+
+window.onload = function makeGame(e) {
+ var tile0 = new Tile(0,2, document.getElementById("tile0"));
+ var tile1 = new Tile(1,2, document.getElementById("tile1"));
+ var tile2 = new Tile(2,2, document.getElementById("tile2"));
+ var tile3 = new Tile(0,1, document.getElementById("tile3"));
+ var tile4 = new Tile(1,1, document.getElementById("tile4"));
+ var tile5 = new Tile(2,1, document.getElementById("tile5"));
+ var tile6 = new Tile(0,0, document.getElementById("tile6"));
+ var tile7 = new Tile(1,0, document.getElementById("tile7"));
+ var tile8 = new Tile(2,0, document.getElementById("tile8"));
+//Create and run a Cell constructor(apply .tile):
+ board = [tile0, tile1, tile2, tile3, 
+          tile4, tile5, tile6, tile7, tile8];
+ emptyTile = tile0;
+ updateBoard(); 
+ document.getElementById('headline').innerHTML = "Tile Slides"; 
+ document.getElementById('gameplay').addEventListener('click', function(e) { 
+    document.getElementById('headline').style.visibility = "hidden"; 
+  }); 
+}
+
+  //Create a 1D array of tiles called board ranging from 0 to 8
+    //create each tile with a constructor, assign evenly distributed (x,y) to each tile according to their 
+    //absolute position so that the tiles appear in 3x3 configuration,
+    //assign tile name to it's corresponding div id
+        //(in css id 0 will have "empty" attribute, ids 1-8 will have corresponding values 1-8)
+        //record cell ids and their subsequent randomized coordinates in currentGame []
+        //$('#grid').append. new divs we create corresponding to ids 1-8
+//define winningGame[]
+
+//create and add an event listener that evaluates the coordinates of 
+//the event target on mouse click:
+//if (math.abs(xa-xe) && math.abs(ya-ye)) < 1
+    //run function swap a and e:
+      //temp = a
+      //a = e
+      //e = temp
+      //record new a,e coordinates and insert to currentGame[]
+
+//function youWin () {
+  //if (currentGame == winningGame) {
+    //console.log = "You Win!";
+    //endgame
+    //offer another game?
+     //if yes, reload page. if no, say thankyou and freeze page
+//};
+
+
+
+
